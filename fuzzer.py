23fbae6915f59fc863ff542460a917da1f1f3ac5
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2018-06-15 16:42:19 +0200

    Add proper fuzzing with logging to file, loop and output

--- fuzzer.py
+++ fuzzer.py
@@ -2,24 +2,61 @@ from scapy.contrib.coap import *
 from scapy.all import *
 from scapy.layers.inet import *
 from scapy.layers.inet6 import *
+import argparse
+import json
+import time
+
+def cli_args():
+    parser = argparse.ArgumentParser(description='CoAP Fuzzer for OpenMotes')
+    parser.add_argument("--dest", dest="dst_address", type=str, required=True)
+    parser.add_argument("--iface", dest="interface", type=str)
+    parser.add_argument("--output", dest="output_file", type=str, required=True)
+    parser.add_argument("--overwrite-output", dest="file_overwrite", action='store_true')
+    return parser.parse_args()
 
 
-fuzz_pattern = fuzz(CoAP(
-    ver=1L,
-    type=RandNum(0, 1),
-    code=RandNum(0, 4),
-    token=RandBin(RandNum(0, 8)),
-    options=[(11L, 'core')],
-    paymark='\xff'+str(RandNum(0, 256))
-))
-fuzz_pattern.show2()
+if __name__ == "__main__":
+    args = cli_args()
+    dst_address = args.dst_address
+    interface = args.interface or "tun0"
+    file_mode = "w" if args.file_overwrite else "a"
+    output_file = open(args.output_file, file_mode)
+    output_file.write(json.dumps({
+        "dst_address": dst_address,
+        "interface": interface
+    }) + "\n")
+    log_output = {}
+    try:
+        num_packets = 1
+        print()
+        while True:
+            sys.stdout.write("\rSending packet %d..." % num_packets)
+            sys.stdout.flush()
+            fuzz_pattern = fuzz(CoAP(
+                ver=1L,
+                type=RandNum(0, 1).__int__(),
+                code=RandNum(1, 4).__int__(),
+                token=RandBin(RandNum(0, 8)).__bytes__(),
+                options=[(11L, 'core')],
+                paymark='\xff'+str(RandNum(0, 256).__int__())
+            ))
+            # fuzz_pattern.show2()
+            packet = IPv6(dst=dst_address)/UDP(sport=34552, dport=5683)/fuzz_pattern
+            full_response, empty = sr(packet, iface=interface, timeout=5, verbose=False)
 
-dst_address = "fd00::ff:fe00:5403"
-packet = IPv6(dst=dst_address)/UDP(sport=34552, dport=5683)/fuzz_pattern
-full_response, empty = sr(packet, iface="tun0", timeout=5)
-num_responses = len(full_response)
-if num_responses == 0:  # NON
-    pass
-elif num_responses == 1:  # CON, RST
-    request, response = full_response[0]
-    response.show()
+            num_responses = len(full_response)
+            log_output["request"] = linehexdump(fuzz_pattern, dump=True, onlyhex=1)
+            if num_responses == 0:  # timeout
+                pass
+            elif num_responses == 1:
+                request, response = full_response[0]
+                log_output["response"] = linehexdump(response, dump=True, onlyhex=1)
+                # response.show()
+
+            log_output["timestamp"] = int(time.time())
+            output_file.write(json.dumps(log_output) + "\n")
+
+            num_packets += 1
+    except (KeyboardInterrupt, SystemExit):
+        output_file.close()
+        exit()
