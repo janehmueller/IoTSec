23addc13f138e9200a5a033587b70db291539cf6
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2018-06-15 14:06:48 +0200

    Working ipv6 example

--- fuzzer.py
+++ fuzzer.py
@@ -1,20 +1,28 @@
 from scapy.contrib.coap import *
 from scapy.all import *
-from scapy.layers.inet6 import IPv6
+from scapy.layers.inet import *
+from scapy.layers.inet6 import *
 
-random.seed("Let's fuzz CoAP!")
-conf.L3socket = L3RawSocket
-fuzz_pattern = fuzz(CoAP(
-    ver=1L,
-    type=RandNum(0, 1),
-    code=RandNum(0, 4),
-    token=RandBin(RandNum(0, 8)),
-    options=[(11L, 'core')],
-    paymark='\xff'+str(RandBin())
-))
+# conf.L3socket = L3RawSocket
+# fuzz_pattern = fuzz(CoAP(
+#     ver=1L,
+#     type=RandNum(0, 1),
+#     code=RandNum(0, 4),
+#     token=RandBin(RandNum(0, 8)),
+#     options=[(11L, 'core')],
+#     paymark='\xff'+str(RandBin())
+# ))
+# print("show")
+# fuzz_pattern.show()
+# print("show2")
+# fuzz_pattern.show2()
 
-fuzz_pattern.show2()
-
-dst_address = ""
-packet = IPv6(dst=dst_address)/UDP(sport=34552, dport=5683)/fuzz_pattern
-srloop(packet, verbose=20, retry=-1, timeout=2)
+dst_address = "8.8.8.8"
+dst_address = "ipv6.google.com"
+# packet = IPv6(dst=dst_address)/UDP(sport=34552, dport=5683)/fuzz_pattern
+packet = IPv6(dst=dst_address)/ICMPv6EchoRequest()
+# packet = IP(dst=dst_address)/ICMP()
+for answer in sr(packet):
+    print("showing answer: %s" % answer)
+    answer.show()
+# srloop(packet, verbose=20, retry=-1, timeout=2)
