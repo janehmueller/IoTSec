c52d0eea0d49912d59d182e6695545bf033dd7c2
Justin Trautmann <justin.trautmann@student.hpi.de>
2018-06-15 16:36:19 +0200

    well known core test

--- fuzzer.py
+++ fuzzer.py
@@ -3,7 +3,18 @@ from scapy.all import *
 from scapy.layers.inet import *
 from scapy.layers.inet6 import *
 
-
+def test_well_known_core():
+    wkc_message = CoAP(ver=1L, 
+                       type=0, 
+                       code=1, 
+                       token=RandBin(RandNum(0,8)),
+                       options=[(11L, '.well-known/core')])
+    wkc_req = IPv6(dst=dst_address)/UDP(sport=34552, dport=5683)/wkc_message
+    full_response, empty = sr(wkc_req, iface="tun0", timeout=5)
+    if (full_response[0][1].load == '</.well-known/core>;ct=40,</actuators/leds>;title=\"LEDs: ?color='):
+        return True
+    return False
+  
 fuzz_pattern = fuzz(CoAP(
     ver=1L,
     type=RandNum(0, 1),
@@ -14,7 +25,7 @@ fuzz_pattern = fuzz(CoAP(
 ))
 fuzz_pattern.show2()
 
-dst_address = "fd00::ff:fe00:5403"
+dst_address = "fd00::ff:fe00:53b8"
 packet = IPv6(dst=dst_address)/UDP(sport=34552, dport=5683)/fuzz_pattern
 full_response, empty = sr(packet, iface="tun0", timeout=5)
 num_responses = len(full_response)
@@ -23,3 +34,5 @@ if num_responses == 0:  # NON
 elif num_responses == 1:  # CON, RST
     request, response = full_response[0]
     response.show()
+
+print("Mote is working: " + str(test_well_known_core()))
