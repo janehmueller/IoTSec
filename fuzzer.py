690a8fee1a6c7ffa2a5bae1f40cdc84f280dbb5e
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2018-06-15 15:00:19 +0200

    Working coap fuzzing

--- fuzzer.py
+++ fuzzer.py
@@ -3,26 +3,23 @@ from scapy.all import *
 from scapy.layers.inet import *
 from scapy.layers.inet6 import *
 
-# conf.L3socket = L3RawSocket
-# fuzz_pattern = fuzz(CoAP(
-#     ver=1L,
-#     type=RandNum(0, 1),
-#     code=RandNum(0, 4),
-#     token=RandBin(RandNum(0, 8)),
-#     options=[(11L, 'core')],
-#     paymark='\xff'+str(RandBin())
-# ))
-# print("show")
-# fuzz_pattern.show()
-# print("show2")
-# fuzz_pattern.show2()
 
-dst_address = "8.8.8.8"
-dst_address = "ipv6.google.com"
-# packet = IPv6(dst=dst_address)/UDP(sport=34552, dport=5683)/fuzz_pattern
-packet = IPv6(dst=dst_address)/ICMPv6EchoRequest()
-# packet = IP(dst=dst_address)/ICMP()
-for answer in sr(packet):
-    print("showing answer: %s" % answer)
-    answer.show()
-# srloop(packet, verbose=20, retry=-1, timeout=2)
+fuzz_pattern = fuzz(CoAP(
+    ver=1L,
+    type=RandNum(0, 1),
+    code=RandNum(0, 4),
+    token=RandBin(RandNum(0, 8)),
+    options=[(11L, 'core')],
+    paymark='\xff'+str(RandNum(0, 256))
+))
+fuzz_pattern.show2()
+
+dst_address = "fd00::ff:fe00:5403"
+packet = IPv6(dst=dst_address)/UDP(sport=34552, dport=5683)/fuzz_pattern
+full_response, empty = sr(packet, iface="tun0", timeout=5)
+num_responses = len(full_response)
+if num_responses == 0:  # NON
+    pass
+elif num_responses == 1:  # CON, RST
+    request, response = full_response[0]
+    response.show()
