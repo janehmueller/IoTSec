cdc7af96b5d78a338dffc5029a92bac162a4d660
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2018-07-24 18:00:28 +0200

    Improve setup scripts

--- border_router_setup.sh
+++ border_router_setup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+contiki_path=$1
 cwd=$(pwd)
-echo "change dir"
-cd ../contiki-ng-konrad/examples/rpl-border-router
-echo "set target"
-make TARGET=openmote-cc2538 savetarget
-echo "set board revision"
-echo "BOARD_REVISION = REV_A1" >> Makefile.target
-echo "upload"
-make border-router.upload
-echo "connect"
-make connect-router | ts | tee $cwd/border_router.log
+echo "Changing directory..."
+cd "$contiki_path/examples/rpl-border-router"
+echo "Activating python environment..."
+source activate iot
+make clean
+echo "Setting target...." && \
+    make TARGET=openmote-cc2538 savetarget && \
+    echo "Setting board revision..." && \
+    echo "BOARD_REVISION = REV_A1" >> Makefile.target && \
+    echo "Uploading..." && \
+    make border-router.upload && \
+    echo "Connecting to router..." && \
+    make connect-router | ts | tee $cwd/border_router.log
--- coap_server_setup.sh
+++ coap_server_setup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+contiki_path=$1
 cwd=$(pwd)
-echo "change dir"
-cd ../contiki-ng-konrad/examples/coap/coap-example-server
-echo "set target"
-make TARGET=openmote-cc2538 savetarget
-echo "set board revision"
-echo "BOARD_REVISION = REV_A1" >> Makefile.target
-echo "upload"
-make coap-example-server.upload MOTE=1
-echo "connect"
-make login MOTE=1 | ts | tee $cwd/coap_server.log
+echo "Changing directory..."
+cd "$contiki_path/examples/coap/coap-example-server"
+echo "Activating python environment..."
+source activate iot
+make clean
+echo "Setting target...." && \
+    make TARGET=openmote-cc2538 savetarget && \
+    echo "Setting board revision..." && \
+    echo "BOARD_REVISION = REV_A1" >> Makefile.target && \
+    echo "Uploading..." && \
+    make coap-example-server.upload MOTE=1 && \
+    echo "Connecting to router..." && \
+    make login MOTE=1 | ts | tee $cwd/coap_server.log
